3c4c8d0f36f92423de580f497bd482ed16c0ac2b
齋藤千種 <chitanemaru@gmail.com>
2025-12-24 09:27:48 +0900

    アップロード

---
+++ patterns/cv%e3%82%a8%e3%83%aa%e3%82%a2.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: CVエリア
+ * Slug: imake-theme/cv%e3%82%a8%e3%83%aa%e3%82%a2
+ * Categories: CTA (行動喚起)
+ */
+?>
+<!-- wp:group {"className":"cv-area","style":{"background":{"backgroundImage":{"url":"http://prac.image-make.com/wp-content/uploads/2025/03/cv_bg.jpg","id":553,"source":"file","title":"cv_bg"},"backgroundSize":"cover"}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group cv-area"><!-- wp:heading {"textAlign":"center","className":"section-title"} -->
+<h2 class="wp-block-heading has-text-align-center section-title">お見積り・お問合わせ</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"className":"card-wrapper","style":{"spacing":{"blockGap":"7%"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group card-wrapper"><!-- wp:block {"ref":446} /-->
+
+<!-- wp:block {"ref":448} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/tel.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: tel
+ * Slug: imake-theme/tel
+ * Categories: 
+ */
+?>
+<!-- wp:group {"className":"card","style":{"spacing":{"padding":{"top":"24px","bottom":"24px"}}},"backgroundColor":"white","layout":{"type":"constrained"}} -->
+<div class="wp-block-group card has-white-background-color has-background" style="padding-top:24px;padding-bottom:24px"><!-- wp:heading {"textAlign":"center","level":3} -->
+<h3 class="wp-block-heading has-text-align-center">お電話で相談する</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","className":"tel-no"} -->
+<p class="has-text-align-center tel-no"><a href="tel:0467539376">046-753-9376</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","className":"opening-hour"} -->
+<p class="has-text-align-center opening-hour">受付時間：月-土 9時-19時</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
